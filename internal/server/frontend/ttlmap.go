e839afedcecebe0d786e0353191e9f095a1a01d8
ekotlikoff <ekotlikoff@gmail.com>
2025-11-09 15:44:39 -0500

    Clean up unlocks

--- internal/server/frontend/ttlmap.go
+++ internal/server/frontend/ttlmap.go
@@ -49,6 +49,7 @@ func (m *TTLMap) Len() int {
 // Put puts key k and value v
 func (m *TTLMap) Put(k string, v *tableserver.Player) error {
 	m.l.Lock()
+	defer m.l.Unlock()
 	_, ok := m.m[k]
 	if !ok {
 		it := &item{value: v}
@@ -57,20 +58,19 @@ func (m *TTLMap) Put(k string, v *tableserver.Player) error {
 	} else {
 		return errors.New("failed to put key: " + k + ", value: " + v.GetName())
 	}
-	m.l.Unlock()
 	return nil
 }
 
 // Get gets value for key k
 func (m *TTLMap) Get(k string) (v *tableserver.Player, err error) {
 	m.l.Lock()
+	defer m.l.Unlock()
 	if it, ok := m.m[k]; ok {
 		v = it.value
 		it.lastAccess = time.Now().Unix()
 	} else {
 		err = errors.New("failed to get")
 	}
-	m.l.Unlock()
 	return
 
 }
@@ -78,6 +78,7 @@ func (m *TTLMap) Get(k string) (v *tableserver.Player, err error) {
 // Refresh updates the key k to newk
 func (m *TTLMap) Refresh(k, newk string) error {
 	m.l.Lock()
+	defer m.l.Unlock()
 	it, ok := m.m[k]
 	if ok {
 		it.lastAccess = time.Now().Unix()
@@ -88,6 +89,5 @@ func (m *TTLMap) Refresh(k, newk string) error {
 	} else {
 		return errors.New("failed to refresh key")
 	}
-	m.l.Unlock()
 	return nil
 }
